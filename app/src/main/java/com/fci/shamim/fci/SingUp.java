e7390aafba01d3f0d30daf7775422a90a6bbb232
Hridoy <hridoykrisnadas@gmail.com>
2019-09-27 17:12:57 +0600

    Find ID on Signup Java Class

--- app/src/main/java/com/fci/shamim/fci/SingUp.java
+++ app/src/main/java/com/fci/shamim/fci/SingUp.java
@@ -4,14 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class SingUp extends AppCompatActivity {
 
-    EditText nameEt;
+    EditText nameEt, emailEt, phoneEt, presAddressEt, permaAddressEt, skillEt, universityEt, subjectEt, designationEt, officeEt, locationEt, jobSectorEt;
+
+    Spinner departmentSp, batchSp, shiftsp, bloodSp, genderSp, degreeSp, passingYearSp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sing_up);
+
+
+        /* ---------------Edit Text ID Find---------------------------*/
+
+        nameEt = findViewById(R.id.UsernameSignUpId);
+        emailEt = findViewById(R.id.emailSignUpId);
+        phoneEt = findViewById(R.id.phoneId);
+        presAddressEt = findViewById(R.id.presentAddressId);
+        permaAddressEt = findViewById(R.id.permanenttAddressId);
+        skillEt = findViewById(R.id.skillId);
+        universityEt = findViewById(R.id.universityId);
+        subjectEt = findViewById(R.id.subjectId);
+        designationEt = findViewById(R.id.designationId);
+        officeEt = findViewById(R.id.officeNameId);
+        locationEt = findViewById(R.id.officeLocationId);
+        jobSectorEt = findViewById(R.id.jobSectorId);
+
+        /* ---------------Spinner ID Find---------------------------*/
+
+        departmentSp = findViewById(R.id.selectDepartmentId);
+        batchSp = findViewById(R.id.selectBatchId);
+        shiftsp = findViewById(R.id.selectShiftId);
+        bloodSp = findViewById(R.id.selectBloodId);
+        genderSp = findViewById(R.id.selectGenderId);
+        degreeSp = findViewById(R.id.selectDegreeId);
+        passingYearSp = findViewById(R.id.selectPassingYearId);
+
+
     }
 }
